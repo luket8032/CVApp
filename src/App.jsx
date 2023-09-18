8629ae3fe7fe324b7b461754fc787e263cae03fd
Luke <luket8032@hotmail.com>
2023-09-18 16:22:42 -0500

    Create cv preview template component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Forms from './components/Forms.jsx';
+import Preview from './components/Preview.jsx';
 import './styles/app.css'
 
 function App() {
 
   return (
-    <>
-    <Forms />
-    </>
+    <main>
+      <Forms />
+      <Preview />
+    </main>
   )
 }
 
---
+++ src/components/Preview.jsx
@@ -0,0 +1,11 @@
+import '../styles/preview.css'
+
+function Preview() {
+    return (
+        <div className="preview-wrapper">
+            
+        </div>
+    )
+}
+
+export default Preview
