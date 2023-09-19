9fefe193cf28ebaed62a724fdfd5965b720141d5
Luke <luket8032@hotmail.com>
2023-09-19 14:41:56 -0500

    Add experience form component on button click

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,6 +4,13 @@ import '../styles/dropdown.css'
 
 function Education () {
     const [isOpen, setOpen] = useState(false);
+    const [keyCount, setKeyCount] = useState(0)
+    const [experienceList, setExperienceList] = useState([])
+
+    function addExperience() {
+        setKeyCount(prevKey => prevKey + 1);
+        setExperienceList([...experienceList, {id: keyCount}]);
+    }
 
     return (
         <div className='dropdown-section-wrapper'>
@@ -13,8 +20,9 @@ function Education () {
                 <img className='dropdown-icon' src="/dropdown.svg" alt="dropdown icon" onClick={()=> setOpen(!isOpen)} />
             </div>
             <div className={`dropdown-list ${isOpen ? 'active' : 'inactive'}`}>
-                <ExperienceForm />
+                {experienceList.map(item => <ExperienceForm key={item.id}/>)}
             </div>
+            <button className={`experience-btn ${isOpen ? 'active' : 'inactive'}`} onClick={addExperience}>Add experience</button>
         </div>
     )
 }
