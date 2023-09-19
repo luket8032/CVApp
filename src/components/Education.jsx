f78be4178bbfd5cf8a9ed23af40ab7de22a1a778
Luke <luket8032@hotmail.com>
2023-09-19 14:30:46 -0500

    Add useState to add new education form

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,6 +4,13 @@ import '../styles/dropdown.css'
 
 function Education () {
     const [isOpen, setOpen] = useState(false);
+    const [keyCount, setKeyCount] = useState(0)
+    const [educationList, setEducationList] = useState([])
+
+    function addEducation() {
+        setKeyCount(prevKey => prevKey + 1);
+        setEducationList([...educationList, {id: keyCount}]);
+    }
 
     return (
         <div className='dropdown-section-wrapper'>
@@ -13,8 +20,9 @@ function Education () {
                 <img className='dropdown-icon' src="/dropdown.svg" alt="dropdown icon" onClick={()=> setOpen(!isOpen)} />
             </div>
             <div className={`dropdown-list ${isOpen ? 'active' : 'inactive'}`}>
-                <EducationForm />
+                {educationList.map(item => <EducationForm key={item.id}/>)}
             </div>
+            <button className={`education-btn ${isOpen ? 'active' : 'inactive'}`} onClick={addEducation}>Add education</button>
         </div>
     )
 }
--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -4,30 +4,30 @@ import '../styles/form.css'
 function EducationForm () {
     return (
         <form>
+            <div>
+                <label htmlFor="school">School</label>
+                <input type="text" id='school' name='school'/>
+            </div>
+            <div>
+                <label htmlFor="degree">Degree</label>
+                <input type="text" id='degree' name='degree'/>
+            </div>
+            <div className="date-inputs">
                 <div>
-                    <label htmlFor="school">School</label>
-                    <input type="text" id='school' name='school'/>
-                </div>
-                <div>
-                    <label htmlFor="degree">Degree</label>
-                    <input type="text" id='degree' name='degree'/>
-                </div>
-                <div className="date-inputs">
-                    <div>
-                        <label htmlFor="startDate">Start Date</label>
-                        <input type="date" id='startDate' name='startDate'/>
-                    </div>
-                    <div>
-                        <label htmlFor="endDate">End Date</label>
-                        <input type="date" id='endDate' name='endDate'/>
-                    </div>
+                    <label htmlFor="startDate">Start Date</label>
+                    <input type="date" id='startDate' name='startDate'/>
                 </div>
                 <div>
-                    <label htmlFor="location">Location</label>
-                    <input type="text" id='location' name='location'/>
+                    <label htmlFor="endDate">End Date</label>
+                    <input type="date" id='endDate' name='endDate'/>
                 </div>
-                <FormButtons />
-            </form>
+            </div>
+            <div>
+                <label htmlFor="location">Location</label>
+                <input type="text" id='location' name='location'/>
+            </div>
+            <FormButtons />
+        </form>
     )
 }
 
