c94912ba02f31ca879b3d51c67311e9f2c5a213e
Luke <luket8032@hotmail.com>
2023-09-21 10:31:28 -0500

    Add dropdown button to general form section

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,7 +1,8 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import '../styles/form.css';
 
 function General({onChange}) {
+    const [isOpen, setOpen] = useState(true);
 
     function handleChange() {
         const name = document.getElementById('fullName').value;
@@ -22,22 +23,27 @@ function General({onChange}) {
     return (
         <div className='form-container'>
             <form action="submit">
-                <h2><img src="/person.svg" alt="person icon" />General Information</h2>
-                <div>
-                    <label htmlFor="fullName">Full Name</label>
-                    <input type="text" id='fullName' name='fullName' onChange={handleChange}/>
+                <div className="title">
+                    <h2><img src="/person.svg" alt="person icon" />General Information</h2>
+                    <img className='dropdown-icon' src="/dropdown.svg" alt="dropdown icon" onClick={()=> setOpen(!isOpen)} />
                 </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id='email' name='email' onChange={handleChange}/>
-                </div>
-                <div>
-                    <label htmlFor="phoneNumber">Phone number</label>
-                    <input type="tel" id='phoneNumber' name='phoneNumber' onChange={handleChange}/>
-                </div>
-                <div>
-                    <label htmlFor="address">Address</label>
-                    <input type="text" id='address' name='address' onChange={handleChange}/>
+                <div className={`form-section ${isOpen ? 'active' : 'inactive'}`}>
+                    <div>
+                        <label htmlFor="fullName">Full Name</label>
+                        <input type="text" id='fullName' name='fullName' onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' name='email' onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="phoneNumber">Phone number</label>
+                        <input type="tel" id='phoneNumber' name='phoneNumber' onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="address">Address</label>
+                        <input type="text" id='address' name='address' onChange={handleChange}/>
+                    </div>
                 </div>
             </form>
         </div>
